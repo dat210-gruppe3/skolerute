e1cdbbfed796163dac9edd5fa741a604ff8e290f
Endre Helland <en.helland@stud.uis.no>
2016-10-03 11:24:11 +0200

    Midlertidig fix

--- skolerute/skolerute/Views/StartUpPage.xaml.cs
+++ skolerute/skolerute/Views/StartUpPage.xaml.cs
@@ -12,48 +12,79 @@ namespace skolerute.Views
 	public partial class StartUpPage : ContentPage
 	{
 		public StartUpPage()
-
 		{
 			//http://blog.alectucker.com/post/2015/08/10/xamarinforms-searchbar-with-mvvm.aspx
 
 			SchoolList skolelisten = Init();
 			BindingContext = skolelisten;
 			InitializeComponent();
-			//skoler.ItemsSource = new List<data.School>() {new data.School("Gosen", null)};
-			db.DatabaseManager database = new db.DatabaseManager();
-			List<data.School> debugskoler = database.GetSchools().ToList();
-			skoler.ItemsSource = debugskoler;
+            List<School> lista = new List<School>() { new School("Gosen", null) };
+            skoler.ItemsSource = lista;
+            List<School> debugskoler = new List<School>();
 
-			//Dette krasjer programmet totalt.
-			//if (searchSchool.IsFocused)
-			//{
-			//	skoler.ItemsSource = debugskoler;
-			//}
+            try
+            {
+                db.DatabaseManager database = new db.DatabaseManager();
+                debugskoler = database.GetSchools().ToList();
+                skoler.ItemsSource = debugskoler;
+            } catch (Exception e)
+            {
+                lista.Add(new School("Error.", null));
+                skoler.ItemsSource = lista;
+                debugskoler = lista;
+            }
 
 			searchSchool.SearchButtonPressed += (s, e) => {
-                List<data.School> newSchList = new List<data.School>();
-                var sValue = searchSchool.Text;
-				for (int i = 0; i < debugskoler.Count; i++) 
-				{
-                    if (debugskoler[i].name.StartsWith(sValue))
-					{
-						newSchList.Add(debugskoler[i]);
-	        		}
-                    else if (debugskoler[i].name.Contains(sValue))
-                    {
-                        newSchList.Add(debugskoler[i]);
+                List<School> newSchList = new List<School>();
+                var sValue = searchSchool.Text.Trim();
+                if (sValue == "")
+                {
+                    skoler.ItemsSource = debugskoler;
+                } else { 
+                    for (int i = 0; i < debugskoler.Count; i++) 
+				    {
+                        if (debugskoler[i].name.Contains(sValue))
+					    {
+						    newSchList.Add(debugskoler[i]);
+	        		    }
                     }
+				    skoler.ItemsSource = newSchList;
                 }
-				skoler.ItemsSource = newSchList;
-			};
+            };
 
 
+            //Prøver en liten 'autocomplete' basert på samme konsept
+            searchSchool.TextChanged += (s, e) =>
+            {
+                List<data.School> newSchList = new List<data.School>();
+                var sValue = searchSchool.Text.Trim();
+                if (sValue == "")
+                {
+                    skoler.ItemsSource = debugskoler;
+                }
+                else
+                {
+                    for (int i = 0; i < debugskoler.Count; i++)
+                    {
+                        if (debugskoler[i].name.Contains(sValue))
+                        {
+                            newSchList.Add(debugskoler[i]);
+                            skoler.ItemsSource = newSchList;
+                        }
+                    }
+                    
+                }
+            };
 
-
-
+            
 		}
 
-		private SchoolList Init()
+        public void ac(object o, TextChangedEventArgs a)
+        {
+
+        }
+
+        private SchoolList Init()
 		{
 			return new data.SchoolList { liste = new List<string>() { "Kannik", "Sandnes", "Kongsgård" } };
 		}
