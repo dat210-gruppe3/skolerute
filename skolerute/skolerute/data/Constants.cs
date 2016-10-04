a0c2262478f9f7727713871319a8fdbe0f23c167
danielbarati <daniel-barati@hotmail.com>
2016-10-04 12:50:19 +0200

    minor changes

---
+++ skolerute/skolerute/data/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace skolerute
+{
+	public class Constants
+	{
+		public Constants()
+		{
+		}
+
+		public const string URL = "http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv";
+
+	}
+}
