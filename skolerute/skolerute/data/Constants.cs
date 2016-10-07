9711039f140675d71d9b5b10d1bf488c31ba6137
Endre Helland <en.helland@stud.uis.no>
2016-10-07 15:25:47 +0200

    Fixed colors...

--- skolerute/skolerute/data/Constants.cs
+++ skolerute/skolerute/data/Constants.cs
@@ -12,11 +12,11 @@ namespace skolerute
 		public const string URL = "http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv";
 
         //Colors
-        public static Color yellow = Color.Yellow;
-        public static Color blue = Color.Navy;
-        public static Color purple = Color.Purple;
-        public static Color teal = Color.Teal;
-        public static Color maroon = Color.Maroon;
-        public List<Color> colors = new List<Color>() { yellow, blue, purple, teal, maroon };
+        static Color yellow = Color.Yellow;
+        static Color blue = Color.Navy;
+        static Color purple = Color.Purple;
+        static Color teal = Color.Teal;
+        static Color maroon = Color.Maroon;
+        public static List<Color> colors = new List<Color>() { yellow, blue, purple, teal, maroon };
     }
 }
