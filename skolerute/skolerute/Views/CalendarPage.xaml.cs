f16539fe9fcb4092aec0f2dfd41e83dee15070ba
admonish <thomas.johnsrud@gmail.com>
2016-10-24 11:20:14 +0200

    Implementerte fjerning av skoler fra favoritter

--- skolerute/skolerute/Views/CalendarPage.xaml.cs
+++ skolerute/skolerute/Views/CalendarPage.xaml.cs
@@ -33,7 +33,13 @@ namespace skolerute.Views
 				favoriteSchoolNames.Add(args.name);
                 SchoolPicker.ItemsSource = favoriteSchoolNames;
 			});
-			
+
+            MessagingCenter.Subscribe<StartUpPage, School>(this, "deleteSch", (sender, args) =>
+            {
+                favoriteSchoolNames.Remove(args.name);
+                SchoolPicker.ItemsSource = favoriteSchoolNames;
+            });
+
         }
 
         protected async override void OnAppearing()
