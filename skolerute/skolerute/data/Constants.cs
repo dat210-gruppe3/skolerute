78132952623692b94528db71020a59a0c728e821
endreh <en.helland@stud.uis.no>
2016-11-10 14:58:34 +0100

    awesome urls

--- skolerute/skolerute/data/Constants.cs
+++ skolerute/skolerute/data/Constants.cs
@@ -8,9 +8,9 @@ namespace skolerute.data
         // Selecte Schools
         public const int MaximumSelectedSchools = 6;
 
-        // CSV Parser
-		public const string URL = "http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv";
-		public const string positionURL = "http://open.stavanger.kommune.no/dataset/8f8ac030-0d03-46e2-8eb7-844ee11a6203/resource/0371a1db-7074-4568-a0cc-499a5dccfe98/download/skoler.csv";
+        // CSV Parser 
+		public const string URL = "http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/7472239b-e18e-47c4-9810-780240122552/download/skolerute.csv";
+		public const string positionURL = "http://open.stavanger.kommune.no/dataset/8f8ac030-0d03-46e2-8eb7-844ee11a6203/resource/356f485c-e200-45b5-9929-5035d165c394/download/skoler.csv";
 
         // Calendar
         public const int ShownCalendarDaysCount = 42;
