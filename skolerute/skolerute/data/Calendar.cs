6751d7ac587788c6f04388669ea832093a461ff5
danielbarati <daniel-barati@hotmail.com>
2016-11-11 14:45:41 +0100

    moved monthToString to data.Calendar-class

--- skolerute/skolerute/data/Calendar.cs
+++ skolerute/skolerute/data/Calendar.cs
@@ -36,6 +36,38 @@ namespace skolerute.data
 			return dow;
 		}
 
+		public static string MonthToString(int i)
+		{
+			switch (i)
+			{
+				case 1:
+					return "Januar";
+				case 2:
+					return "Februar";
+				case 3:
+					return "Mars";
+				case 4:
+					return "April";
+				case 5:
+					return "Mai";
+				case 6:
+					return "Juni";
+				case 7:
+					return "Juli";
+				case 8:
+					return "August";
+				case 9:
+					return "September";
+				case 10:
+					return "Oktober";
+				case 11:
+					return "November";
+				case 12:
+					return "Desember";
+			}
+			return "";
+		}
+
 		/// <summary>
 		/// Supposed to take in values for year and month, and give out whatever necessary to render the
 		/// calender in the GUI.
