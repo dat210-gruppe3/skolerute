cff1fdaff06573f199241d6a7b6e85cf4f51e002
STranoey <s.tranoy@stud.uis.no>
2016-11-14 14:53:31 +0100

    MonthToString back

--- skolerute/skolerute/data/Calendar.cs
+++ skolerute/skolerute/data/Calendar.cs
@@ -147,7 +147,37 @@ namespace skolerute.data
             return relevantDays;
         }
 
-
+        public static string MonthToString(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return "Januar";
+                case 2:
+                    return "Februar";
+                case 3:
+                    return "Mars";
+                case 4:
+                    return "April";
+                case 5:
+                    return "Mai";
+                case 6:
+                    return "Juni";
+                case 7:
+                    return "Juli";
+                case 8:
+                    return "August";
+                case 9:
+                    return "September";
+                case 10:
+                    return "Oktober";
+                case 11:
+                    return "November";
+                case 12:
+                    return "Desember";
+            }
+            return "";
+        }
 
         public static ObservableCollection<GroupedFreeDayModel> AddSchoolToList(List<School> skoler)
         {
