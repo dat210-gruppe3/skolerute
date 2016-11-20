2862acae14248030c8081f5fb033baf333431a8f
danielbarati <daniel-barati@hotmail.com>
2016-11-20 18:59:55 +0100

    endringer av farger

---
+++ skolerute/skolerute.iOS/exportCalendar/ExportCalendar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EventKit;
+using UIKit;
+using Foundation;
+using EventKitUI;
+namespace skolerute.iOS
+{
+	public class ExportCalendar : IExportCalendar
+	{
+
+		public void ExportToCalendar(string title, string description, string reminder)
+		{
+
+		}
+
+		public void RemoveFromCalendar()
+		{
+		}
+
+	}
+}
